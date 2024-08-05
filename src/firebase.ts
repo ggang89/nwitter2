8be733db4a56ff51484cd5c9f7ceb4796e7901a8
ggang89 <ggang89@gmail.com>
2024-08-05 21:31:26 +0900

    firebase 초기화 연결(firebase설치, 코드 붙여넣음)

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmvXU-8PkSfpbG7zVSMsrO_GSBWO32u-Y",
+  authDomain: "nwitter2-fc631.firebaseapp.com",
+  projectId: "nwitter2-fc631",
+  storageBucket: "nwitter2-fc631.appspot.com",
+  messagingSenderId: "647021443845",
+  appId: "1:647021443845:web:c13bc15906bc63427c556c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
