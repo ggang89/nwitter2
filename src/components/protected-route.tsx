a0b6d023e548d290e2784f5a8cd391076f466fc6
ggang89 <ggang89@gmail.com>
2024-08-18 17:00:19 +0900

    React.ReactNode

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -5,6 +5,7 @@ export default function ProtectedRoute({
   children,
 }: {
     children: React.ReactNode;
+  // React.ReactNode => 컴포넌트의 자식을 설명하는 두가지 방법 중 하나
   }) {
   const user = auth.currentUser;
   if (user === null) {
